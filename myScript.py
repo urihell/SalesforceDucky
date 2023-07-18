e0aefb702baaa97925a0f6fd67f9ea8b36ea89f5
Uriel D <urihell@gmail.com>
2023-07-17 20:28:18 -0400

    Update myScript.py

--- myScript.py
+++ myScript.py
@@ -1,29 +1,38 @@
 import requests
+import argparse
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Script for updating a custom field in Salesforce.')
+parser.add_argument('-u', '--username', help='Salesforce username')
+parser.add_argument('-p', '--password', help='Salesforce password')
+parser.add_argument('-id', '--client_id', help='Salesforce client ID')
+parser.add_argument('-secret', '--client_secret', help='Salesforce client secret')
+args = parser.parse_args()
 
 # Authenticate and get the bearer token
 auth_url = 'https://login.salesforce.com/services/oauth2/token'
 data = {
     'grant_type': 'password',
-    'username': 'udabby@powerorg23.demo',
-    'password': 'salesforce1',
-    'client_id': '3MVG9X12xD2kqQmY5q_tRorE.ODzCcfNF9Xu0a20TUOl09W.9UhPxXiQXlFQuhhPdwqGpNDZe8kLbQnu8pfEz',
-    'client_secret': 'BA5FDAC2D4DE197B4F68041BEA4D85236E8BE08C91047EACA228966B6F0E03D9'
+    'username': args.username,
+    'password': args.password,
+    'client_id': args.client_id,
+    'client_secret': args.client_secret
 }
 response = requests.post(auth_url, data=data)
 
 if response.status_code == 200:
     bearer_token = response.json().get('access_token')
     # Use the bearer token to update the custom field
-    update_url = 'https://udabby-230307-341-demo.my.salesforce.com//services/data/v58.0/sobjects/Asset/02iDm000001Ar2UIAS'
+    update_url = 'https://udabby-230307-341-demo.my.salesforce.com/services/data/v58.0/sobjects/Asset/02iDm000001Ar2UIAS'
     headers = {
         'Authorization': f'Bearer {bearer_token}',
         'Content-Type': 'application/json'
     }
     payload = {
-        'UD_Test__c': true
+        'UD_Test__c': True
     }
     update_response = requests.patch(update_url, headers=headers, json=payload)
-    
+
     if update_response.status_code == 204:
         print('Custom field updated successfully.')
     else:
